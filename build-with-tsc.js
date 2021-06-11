d02abfce36a703b469be7f927d88e623d3eedf09
hanqing <hanq@qq.com>
2021-06-11 11:50:31 +0800

    update

--- build-with-tsc.js
+++ build-with-tsc.js
@@ -1,14 +1,16 @@
 const path = require('path')
 const fs = require('fs')
+const shelljs = require('shelljs')
 const args = process.argv.slice(2)
 const params = {}
 for (let i = 0; i < args.length; i += 2) {
   params[args[i].replace('--', '')] = args[i + 1]
 }
-const destination = path.join(__dirname, 'temp_tsconfig.json')
+
+const TEMP_TSCONFIG_NAME = 'temp_tsconfig.json'
+const destination = path.join(__dirname, TEMP_TSCONFIG_NAME)
 const tsconfig = require('./tsconfig.json')
 tsconfig.include = [`${params.dir}/**/*`]
-
 fs.writeFileSync(destination, JSON.stringify(tsconfig, null, 2))
 
-console.log('temp_tsconfig.json')
+shelljs.exec('npx tsc -p ' + TEMP_TSCONFIG_NAME)
