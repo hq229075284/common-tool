33e6fcf7aad10a97afc56242784898b07e625be7
hanqing <hanq@qq.com>
2021-06-11 15:50:06 +0800

    move

--- build-with-tsc.js
+++ build-with-tsc.js
@@ -10,7 +10,7 @@ for (let i = 0; i < args.length; i += 2) {
 const TEMP_TSCONFIG_NAME = 'temp_tsconfig.json'
 const destination = path.join(__dirname, TEMP_TSCONFIG_NAME)
 const tsconfig = require('./tsconfig.json')
-tsconfig.include = [`${params.dir}/**/*`]
+tsconfig.include = params.dir ? [`src/${params.dir}/**/*`] : ['src/**/*']
 fs.writeFileSync(destination, JSON.stringify(tsconfig, null, 2))
 
 shelljs.exec('npx tsc -p ' + TEMP_TSCONFIG_NAME)
