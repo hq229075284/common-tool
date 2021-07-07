cae498ac5fe8b8754cd8a278df28bc8f3b3a3dc4
hanqing <hanq@qq.com>
2021-07-07 18:01:51 +0800

    publish v0.0.6

--- src/ajax/abstract.ts
+++ src/ajax/abstract.ts
@@ -1,42 +1,53 @@
-import type { AxiosError, AxiosInstance, AxiosPromise, AxiosRequestConfig, AxiosResponse } from 'axios'
+import type { AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse, CancelTokenSource } from 'axios'
 
 import Axios from 'axios'
 
-export interface IBaseAXiosConfig extends AxiosRequestConfig {
-  onceAtSameTime?: boolean
-  getCancelTokenKey?(config: IBaseAXiosConfig): any
-}
-
-export interface ICompleteAxiosConfig<T = any> extends IBaseAXiosConfig {
-  onSuccess(res: ResponseData<T>): void
-  onFail(res: AxiosError<ResponseData>): void
-}
-
-export type AllowedRequestMethod = 'POST' | 'GET' | 'PUT' | 'DELETE'
-
 export type ResponseData<T = any> = {
+  errmsg: string
   message: T
   status: number
 }
 
+export interface IBaseAxiosConfig extends AxiosRequestConfig {
+  onceAtSameTime?: boolean
+  getCancelTokenKey?(config: unknown): any
+}
+
+export interface IPromiseAxiosConfig extends IBaseAxiosConfig {
+  customErrorHandler?: boolean
+}
+export type IPromiseAxiosThenValue<T> = { data: ResponseData<T>; config: IPromiseAxiosConfig }
+export type IPromiseAxiosErrorValue = { error: AxiosError<ResponseData>; config: IPromiseAxiosConfig }
+
+export interface ICallBackAxiosConfig<T> extends IBaseAxiosConfig {
+  onSuccess(res: ResponseData<T>, conifg: ICallBackAxiosConfig<T>): void
+  onFail(res: AxiosError<ResponseData>, conifg: ICallBackAxiosConfig<T>): void
+}
+
+export const PENDING = new Promise<never>(() => {})
+
+export type AllowedRequestMethod = 'POST' | 'GET' | 'PUT' | 'DELETE'
+
 export default class Request {
   protected instance: AxiosInstance
-  protected PENDING = new Promise<never>(() => {})
-  protected cancelTokenMap = new Map()
-  constructor(baseCofig: IBaseAXiosConfig = { onceAtSameTime: true }) {
+
+  public cancelTokenMap = new Map<any, CancelTokenSource>()
+
+  constructor(baseCofig: IBaseAxiosConfig = { onceAtSameTime: true }) {
     this.instance = Axios.create(baseCofig)
   }
-  _createAjax<T>(url: string, method: AllowedRequestMethod = 'POST') {
+
+  protected _createAjax<T>(url: string, method: AllowedRequestMethod = 'POST') {
     const _this = this
-    function fn(data: any, extraConfig: ICompleteAxiosConfig<T>): Promise<void>
-    function fn(data: any, extraConfig: IBaseAXiosConfig): Promise<ResponseData<T>>
-    function fn(this: Request, data, extraConfig: IBaseAXiosConfig | ICompleteAxiosConfig<T>): Promise<void> | Promise<ResponseData<T>> {
+    function fn(data: any, extraConfig: ICallBackAxiosConfig<T>): Promise<void>
+    function fn(data: any, extraConfig?: IPromiseAxiosConfig): Promise<IPromiseAxiosThenValue<T>>
+    function fn(this: Request, data: any, extraConfig?: IPromiseAxiosConfig | ICallBackAxiosConfig<T>): any {
       return _this.runRequest<T>(url, method, data, extraConfig)
     }
 
     // type IFn = {
-    //   (data: any, extraConfig: ICompleteAxiosConfig<T>): Promise<void>
-    //   (data: any, extraConfig: IBaseAXiosConfig): Promise<ResponseData<T>>
+    //   (data: any, extraConfig: ICallBackAxiosConfig<T>): Promise<void>
+    //   (data: any, extraConfig: IPromiseAxiosConfig): Promise<ResponseData<T>>
     // }
     // const fn = ((data, extraConfig) => {
     //   return this.runRequest<T>(url, method, data, extraConfig)
@@ -45,16 +56,17 @@ export default class Request {
     return fn
   }
 
-  // runRequest<T>(url: string, method: AllowedRequestMethod, data: any, extraConfig: ICompleteAxiosConfig<T>): Promise<void>
-  // runRequest<T>(url: string, method: AllowedRequestMethod, data: any, extraConfig: IBaseAXiosConfig): Promise<ResponseData<T>>
-  runRequest<T>(
+  // runRequest<T>(url: string, method: AllowedRequestMethod, data: any, extraConfig: ICallBackAxiosConfig<T>): Promise<void>
+  // runRequest<T>(url: string, method: AllowedRequestMethod, data: any, extraConfig: IPromiseAxiosConfig): Promise<ResponseData<T>>
+  protected runRequest<T>(
     url: string,
     method: AllowedRequestMethod,
-    data,
-    extraConfig: IBaseAXiosConfig | ICompleteAxiosConfig
-  ): Promise<ResponseData<T>> | Promise<void> {
+    data: any,
+    extraConfig?: IPromiseAxiosConfig | ICallBackAxiosConfig<T>
+  ): Promise<IPromiseAxiosThenValue<T> | void> {
+    // ):  {
     const config = {
-      ...this.instance.defaults,
+      ...(this.instance.defaults as IBaseAxiosConfig),
       url,
       method,
       [method === 'GET' ? 'data' : 'params']: data,
@@ -67,22 +79,23 @@ export default class Request {
       config.cancelToken = source.token
       if (this.cancelTokenMap.has(key)) {
         this.cancelTokenMap
-          .get(key)
+          .get(key)!
           .cancel(`previous request was cancelled to ensure that guarad only one ${key} request existed on the network at the same time`)
       }
       this.cancelTokenMap.set(key, source)
     }
     // <void | ResponseData, void | AxiosError<ResponseData>>
-    let p = this.instance.request(config)
+    const p = this.instance.request(config)
     return p.then(
+      // ()=>{throw new Error('12')},
       (...args) => this.onSuccess<T>(...args, config),
       (...args) => this.onFail(...args, config)
     )
   }
 
-  // test1(config: ICompleteAxiosConfig): null
-  // test1(config: IBaseAXiosConfig): number
-  // test1(config: ICompleteAxiosConfig | IBaseAXiosConfig): null | number {
+  // test1(config: ICallBackAxiosConfig): null
+  // test1(config: IPromiseAxiosConfig): number
+  // test1(config: ICallBackAxiosConfig | IPromiseAxiosConfig): null | number {
   //   if ('onSuccess' in config) {
   //     const c = config
   //     return null
@@ -90,38 +103,33 @@ export default class Request {
   //   return 1
   // }
 
-  private onSuccess<T>(response: AxiosResponse<ResponseData<T>>, config: ICompleteAxiosConfig<T>): void
-  private onSuccess<T>(response: AxiosResponse<ResponseData<T>>, config: IBaseAXiosConfig): ResponseData<T>
-  private onSuccess<T>(response: AxiosResponse<ResponseData<T>>, config: IBaseAXiosConfig | ICompleteAxiosConfig<T>): ResponseData<T> | void {
+  private onSuccess<T>(response: AxiosResponse<ResponseData<T>>, config: ICallBackAxiosConfig<T>): void
+
+  private onSuccess<T>(response: AxiosResponse<ResponseData<T>>, config: IPromiseAxiosConfig): IPromiseAxiosThenValue<T>
+
+  private onSuccess<T>(
+    response: AxiosResponse<ResponseData<T>>,
+    config: IPromiseAxiosConfig | ICallBackAxiosConfig<T>
+  ): IPromiseAxiosThenValue<T> | void {
     const { data } = response
     // https://www.typescriptlang.org/docs/handbook/2/narrowing.html#the-in-operator-narrowing
     if ('onSuccess' in config) {
-      return config.onSuccess(data)
+      return config.onSuccess(data, config)
     }
-    return data
+    return { data, config }
   }
 
-  private onFail(error: AxiosError<ResponseData>, config: ICompleteAxiosConfig): Promise<never> | void
-  private onFail(error: AxiosError<ResponseData>, config: IBaseAXiosConfig): Promise<never> | never
-  private onFail(error: AxiosError<ResponseData>, config: IBaseAXiosConfig | ICompleteAxiosConfig | any): Promise<never> | never | void {
+  private onFail<T = any>(error: AxiosError<ResponseData>, config: ICallBackAxiosConfig<T>): Promise<never> | void
+
+  private onFail(error: AxiosError<ResponseData>, config: IPromiseAxiosConfig): Promise<never>
+
+  private onFail<T = any>(error: AxiosError<ResponseData>, config: IPromiseAxiosConfig | ICallBackAxiosConfig<T>): Promise<never> | void {
     if (Axios.isCancel(error)) {
-      return this.PENDING
+      return PENDING
     }
-    if ('onFail' in config) return config.onFail(error)
-    throw error
+    if ('onFail' in config) {
+      return config.onFail(error, config)
+    }
+    return Promise.reject({ error, config })
   }
 }
-
-// const a = new Request()
-// a.createAjax('', 'POST')(
-//   {},
-//   {
-//     // onSuccess(e) {
-//     //   console.log(e)
-//     // },
-//   }
-// ).then((r) => {
-//   if (r) {
-//     r.status
-//   }
-// })
--- src/ajax/index.ts
+++ src/ajax/index.ts
@@ -1,23 +1,25 @@
-import Request from './abstract'
-import type { IBaseAXiosConfig, ICompleteAxiosConfig, AllowedRequestMethod, ResponseData } from './abstract'
-import type { AxiosError } from 'axios'
+/* eslint-disable */
+import Request, { PENDING } from './abstract'
+import type { IPromiseAxiosConfig, AllowedRequestMethod, ResponseData } from './abstract'
 
 // interface Data {
 //   id: number
 //   name: 'a' | 'b'
 // }
+const SUCCESS = 0
 
 export default class Ajax extends Request {
   // createAjax<T = any>(...args: [string, AllowedRequestMethod]) {
   createAjax<T>(url: string, method: AllowedRequestMethod = 'POST') {
-    const fn = (...others: [any, IBaseAXiosConfig]) => {
-      return super
-        ._createAjax<T>(
-          url,
-          method
-        )(...others)
-        .then((r) => r.message)
-    }
+    const fn = (data: any, extraConfig?: IPromiseAxiosConfig) =>
+      super
+        ._createAjax<T>(url, method)(data, extraConfig)
+        .then((r) => {
+          if (r.data.status === SUCCESS) {
+            return r.data.message
+          }
+          return Promise.reject(r)
+        })
     return fn
   }
 }
