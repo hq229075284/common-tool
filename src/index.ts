92c24dd3cecc577ca1791c7151339145a41337e8
hanqing <229075284@qq.com>
2021-08-15 16:27:14 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,7 @@ import Ajax from './ajax'
 import { PENDING } from './ajax/abstract'
 import Socket from './socket'
 import Cache from './cache'
+import Workflow from './workflow'
+import WorkflowPool from './workflow/loop'
 
-export { Ajax, Socket, Cache, PENDING }
+export { Ajax, Socket, Cache, PENDING, Workflow, WorkflowPool }
