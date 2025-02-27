da3c9eb378f6d29e105edf902232f5b98c5a1372
韩卿 <hanqing@qq.com>
2025-02-27 20:32:30 +0800

    fix: 🐛 ts-jest的配置中指定tsconfig，让ts-jest可以处理js文件

--- jest-preset.js
+++ jest-preset.js
@@ -7,5 +7,9 @@ const puppeteer_preset = require('jest-puppeteer/jest-preset')
 module.exports = merge(ts_preset, puppeteer_preset, {
   globals: {
     // test_url: `http://${process.env.HOST || '127.0.0.1'}:${process.env.PORT || 3000}`,
+    'ts-jest': {
+      // 指定tsconfig，让ts-jest可以处理js文件
+      tsconfig: '<rootDir>/tsconfig.base.json',
+    },
   },
 })
